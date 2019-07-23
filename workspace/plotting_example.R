9a627259a059a8dc1c1ae35e308b44f7f7f6391d
Nate Jones <njones@rstudio03.research.sesync.org>
2019-07-23 18:12:40 -0400

    Added some plots

--- workspace/plotting_example.R
+++ workspace/plotting_example.R
@@ -42,6 +42,7 @@ db<-dbConnect(RSQLite::SQLite(), paste0(working_dir, "choptank.sqlite"))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #2.0 Query ts data for small number of sites------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#2.1 Compare "large" and "small" weltand----------------------------------------
 #List sites in NB
 db_get_sites(db)
 
@@ -81,13 +82,52 @@ legend("bottomright",
       box.lty=0)
 box()
 
+#2.2 Plot Upland and Wetland Water ---------------------------------------------
+#List sites in NB
+db_get_sites(db)
+
+#select time series (in this case, GB and NB to represent size distributions)
+upland  <- db_get_ts(db = db, site_code = "QB Upland Well 1", variable_code_CV = "waterLevel")
+wetland <- db_get_ts(db = db, site_code = "QB Wetland Well Shallow", variable_code_CV = "waterLevel")
+
+#Agregate to daily data
+upland<-upland %>% 
+  mutate(Timestamp=floor_date(Timestamp, "day")) %>% 
+  group_by(Timestamp) %>% 
+  summarise(waterLevel = mean(waterLevel, na.rm=T))
+wetland<-wetland %>% 
+  mutate(Timestamp=floor_date(Timestamp, "day")) %>% 
+  group_by(Timestamp) %>% 
+  summarise(waterLevel = mean(waterLevel, na.rm=T))
+
+#Plot for funzies 
+#plotting parameters
+par(mgp=c(1.5,0.6,0)) 
+par(ps=12)
+par(cex.lab=14/12)
+par(cex.axis=10/12)
+#Start blank plot
+plot(upland, type="n", ylab="Water Depth [m]", xlab="Timestamp")
+#Add line for ground surface
+abline(h=0, lty=2, lwd=2)
+#Add ts data
+points(upland, type="l", col="orangered3", lwd=1.5)
+points(wetland, type="l", col="dodgerblue3", lwd=1.5)
+#Add legend
+legend("bottomright", 
+       c("Wetland", "Upland"),
+       col=c('dodgerblue3','orangered3'), 
+       lty=c(1,1), 
+       cex=12/12, 
+       box.lty=0)
+box()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #3.0 Query ts data for a large number of sites----------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Create download function
 fun<-function(site){
   #download data
-  temp<-db_get_ts(db, paste(site), "waterLevel") %>% as_tibble(.)
+  temp<-db_get_ts(db, paste(site), "waterDepth") %>% as_tibble(.)
   
   #add site collumn
   colnames(temp)<-c("Timestamp", "waterDepth")
@@ -97,6 +137,7 @@ fun<-function(site){
   temp 
 }
 
+#3.1 Plot Aggregate Wetland Data------------------------------------------------
 #Create list of wells to downlaod data for
 site_names<-db_get_sites(db) %>% 
   enframe(.) %>%
@@ -110,18 +151,60 @@ SWL<-lapply(site_names, fun) %>% bind_rows(.)
 #Aggregate Data 
 SWL<-SWL %>%
   mutate(Timestamp=floor_date(Timestamp, "day")) %>% 
-  group_by(Timestamp, site) %>% 
-  summarise(waterDepth = mean(waterDepth, na.rm=T)) %>%
-  filter(Timestamp>mdy("10/1/2017"))
+  group_by(Timestamp) %>% 
+  summarise(upr = quantile(waterDepth, 0.75, na.rm=T), 
+            lwr = quantile(waterDepth, 0.25, na.rm=T), 
+            waterDepth = mean(waterDepth, na.rm=T)) %>%
+  filter(Timestamp>mdy("10/1/2017")) %>%
+  filter(Timestamp<mdy("9/30/2018")) 
 
 #Plot
 SWL %>%
   ggplot(aes(x=Timestamp, y=waterDepth)) +
-    geom_line(aes(color=site)) +
+    geom_ribbon(aes(ymin=lwr, ymax=upr), fill="grey70") +
+    geom_line() +
     scale_color_grey()+
     theme_bw() +
       labs(y="Water Depth [m]")
-    
+
+#Print
+ggsave(paste0(working_dir,"output/aggregate_water_level.png"), width=7, height=5, units="in")
+
+#Plot Aggregate Catchment Data--------------------------------------------------
+#Create list of wells to downlaod data for
+site_names<-db_get_sites(db) %>% 
+  enframe(.) %>%
+  filter(str_detect(value,"Catchment")) %>%
+  select(value) %>%
+  as.matrix(.)
+
+#Download Data
+SWL<-lapply(site_names, fun) %>% bind_rows(.)
+
+#Aggregate Data 
+SWL<-SWL %>%
+  mutate(Timestamp=floor_date(Timestamp, "day")) %>% 
+  mutate(waterDepth = if_else(waterDepth<0, 0, waterDepth)) %>%
+  group_by(Timestamp) %>% 
+  summarise(upr = quantile(waterDepth, 0.75, na.rm=T), 
+            lwr = quantile(waterDepth, 0.25, na.rm=T), 
+            waterDepth = mean(waterDepth, na.rm=T)) %>%
+  filter(Timestamp>mdy("10/1/2017")) %>%
+  filter(Timestamp<mdy("9/30/2018")) 
+
+#Plot
+SWL %>%
+  ggplot(aes(x=Timestamp, y=waterDepth)) +
+  geom_hline(aes(yintercept=0), lty=2) +
+  geom_ribbon(aes(ymin=lwr, ymax=upr), fill="grey70") +
+  geom_line() +
+  scale_color_grey()+
+  theme_bw() +
+  labs(y="Water Depth [m]")
+
+#Print
+ggsave(paste0(working_dir,"output/aggregate_catchment_level.png"), width=3.5, height=3, units="in")
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #4.0 Create interactive plot----------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
